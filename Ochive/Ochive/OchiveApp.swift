fed192dccafd48ebb917f38155b450c072cab2f1
yeeun <nov9ye@ncsoft.com>
2022-11-02 23:07:36 +0900

    FEAT: Make Project

---
+++ Ochive/Ochive/OchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  OchiveApp.swift
+//  Ochive
+//
+//  Created by PARK YEEUN on 2022/11/02.
+//
+
+import SwiftUI
+
+@main
+struct OchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
